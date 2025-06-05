573d166762735d721fe99d981f10fa601b33cd1d
islam22514 <gherbi12islam@gmail.com>
2025-06-05 20:08:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const downloadBtn = document.querySelector(".download-btn");
+const fileLink = "https://drive.usercontent.google.com/download?id=1bB4gpt1yD0Ei4_80UBHRcavvgRfXxQ1K&export=download&authuser=0";
+
+const initTimer = () => {
+    if(downloadBtn.classList.contains("disable-timer")) {
+        return location.href = fileLink;
+    }
+
+    let timer = downloadBtn.dataset.timer;
+    downloadBtn.classList.add("timer");
+    downloadBtn.innerHTML = ` <b>${timer}</b> seconds`;
+    const initCounter = setInterval(() => {
+        if(timer > 0) {
+            timer--;
+            return downloadBtn.innerHTML = `  <b>${timer}</b> seconds`;
+        }
+
+        clearInterval(initCounter);
+        location.href = fileLink;
+        downloadBtn.innerText = "Your file is downloading...";
+        setTimeout(() => {
+            downloadBtn.classList.replace("timer", "disable-timer");
+            downloadBtn.innerHTML = `<span class="icon material-symbols-rounded">vertical_align_bottom</span>
+                                     <span class="text">Download Again/span>`;
+        }, 3000);
+    }, 1000);
+}
+        
+downloadBtn.addEventListener("click", initTimer);
